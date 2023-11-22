5d06bbd00d73709b846d8dabb4e7965ee4b3b819
ismaelgithub7 <ismaelfernandezmendezfp@gmail.com>
2023-11-22 18:27:35 +0100

    test unitario añadidos

--- django-data/calculator_project/calculator_app/tests.py
+++ django-data/calculator_project/calculator_app/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class CalculatorTests(TestCase):
+    def test_home_view_status_code(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_addition(self):
+        data = {'num1': '2', 'num2': '3', 'operation': 'add'}
+        response = self.client.post(reverse('calculate'), data)
+        self.assertContains(response, 'Resultado: 5')
+
+    def test_subtraction(self):
+        data = {'num1': '5', 'num2': '2', 'operation': 'subtract'}
+        response = self.client.post(reverse('calculate'), data)
+        self.assertContains(response, 'Resultado: 3')
+
+    def test_multiplication(self):
+        data = {'num1': '4', 'num2': '3', 'operation': 'multiply'}
+        response = self.client.post(reverse('calculate'), data)
+        self.assertContains(response, 'Resultado: 12')
+
+    def test_division(self):
+        data = {'num1': '8', 'num2': '2', 'operation': 'divide'}
+        response = self.client.post(reverse('calculate'), data)
+        self.assertContains(response, 'Resultado: 4')
+
+    def test_invalid_input(self):
+        data = {'num1': 'abc', 'num2': '2', 'operation': 'add'}
+        response = self.client.post(reverse('calculate'), data)
+        self.assertContains(response, 'Por favor, introduce números válidos.')
