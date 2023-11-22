9cc90e1951773bd0a58ff19d8afdad63d8350b0e
ismaelgithub7 <ismaelfernandezmendezfp@gmail.com>
2023-11-22 19:00:25 +0100

    cambio estructura directorios

--- django-init.sh
+++ django-init.sh
@@ -4,7 +4,6 @@ app_name="calculator_app"
 restore_djangopath=false
 if [ $restore_djangopath = true ];
 then
-    # lsof +D /django-data
     rm -rf /django-data/*
 else
     echo "[INFO] Restore django data mode desactivated"
@@ -14,14 +13,13 @@ if [ -d "/django-data/$project_name" ];
 then
     echo "[INFO] Some project exists"
 else
-    django-admin startproject $project_name
+    django-admin startproject $project_name /django-data
 fi
-python3 /django-data/$project_name/manage.py runserver 0.0.0.0:8000 &
-if [ -d f"/django-data/$project_name/$app_name" ];
+python3 /django-data/manage.py runserver 0.0.0.0:8000 &
+if [ -d f"/django-data/$app_name" ];
 then
     echo "[INFO] App $app_name exists"
 else
-    cd $project_name
-    python3 /django-data/$project_name/manage.py startapp $app_name
+    python3 /django-data/manage.py startapp $app_name
 fi
 tail -f /dev/null
