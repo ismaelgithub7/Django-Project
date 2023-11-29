f16a9dedcda18296055e4d8bf9a05a8975bebf9d
ismaelgithub7 <ismaelfernandezmendezfp@gmail.com>
2023-11-29 11:14:22 +0100

    Archivo configuración postgres configurable desde fuera

--- production/postgres-init-production.sh
+++ production/postgres-init-production.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-./usr/local/bin/docker-entrypoint.sh postgres
+chown -R postgres /postgresql-conf/postgresql.conf
+su postgres
+./usr/local/bin/docker-entrypoint.sh postgres -c config_file=/postgresql-conf/postgresql.conf &
+sleep 10
+chown -R 1000 /postgresql-conf/postgresql.conf
+tail -f /dev/null
 
