4516fd9917c27660d2d52b062a491cedfd5e2734
ismaelgithub7 <ismaelfernandezmendezfp@gmail.com>
2023-11-29 17:49:47 +0100

    migrado imágenes a dockerhub y borrado archivos que ya están en los volumenes (docker-data)

--- production/django-init-production.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sleep 10
-python3 /django-data/manage.py makemigrations
-python3 /django-data/manage.py runserver --settings=calculator_project.settings-production 0.0.0.0:8000
-tail -f /dev/null
--- production/postgres-conf.sql
+++
@@ -1,4 +0,0 @@
-ALTER ROLE postgres SET client_encoding TO 'utf8';
-ALTER ROLE postgres SET default_transaction_isolation TO 'read committed';
-ALTER ROLE postgres SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE pdb TO postgres;
