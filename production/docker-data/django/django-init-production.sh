005b8709de7a175ae15b89b9c5b58171718404ff
ismaelgithub7 <ismaelfernandezmendezfp@gmail.com>
2023-11-30 17:41:06 +0100

    Añadido scrpt wait-for-it para inicio django cuando postgres esté listo, y modificación script inicio django

--- production/docker-data/django/django-init-production.sh
+++ production/docker-data/django/django-init-production.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sleep 10
+/docker-data/wait-for-it.sh postgres-alpine-production:5432
 python3 /django-data/manage.py makemigrations
 python3 /django-data/manage.py runserver --settings=calculator_project.settings-production 0.0.0.0:8000
 gunicorn --bind 0.0.0.0:8000 calculator_project.wsgi:application
