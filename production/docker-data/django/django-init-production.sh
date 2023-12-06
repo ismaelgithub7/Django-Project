4fddf91517cfd3f453d77063e8d76a84f46cb600
ismaelgithub7 <ismaelfernandezmendezfp@gmail.com>
2023-12-06 17:55:43 +0100

    dockerfile listo

--- production/docker-data/django/django-init-production.sh
+++ production/docker-data/django/django-init-production.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /docker-data/wait-for-it.sh postgres-alpine-production:5432
-python3 /django-data/manage.py makemigrationse
+python3 /django-data/manage.py makemigrations
 python3 /django-data/manage.py runserver --settings=calculator_project.settings-production 0.0.0.0:8000
 gunicorn --bind 0.0.0.0:8000 calculator_project.wsgi:application
 tail -f /dev/null
