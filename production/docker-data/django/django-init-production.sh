9e38eb381baf3659ca2a575dfeca8d07f136e91f
ismaelgithub7 <ismaelfernandezmendezfp@gmail.com>
2023-12-06 17:47:15 +0100

    prueba error

--- production/docker-data/django/django-init-production.sh
+++ production/docker-data/django/django-init-production.sh
@@ -2,6 +2,5 @@
 /docker-data/wait-for-it.sh postgres-alpine-production:5432
 python3 /django-data/manage.py makemigrations
 python3 /django-data/manage.py runserver --settings=calculator_project.settings-production 0.0.0.0:8000
-gunicorn --bind 0.0.0.0:8000 calculator_project.wsgi:application
-adsasda
-tailo -f /dev/null
+gunicorno --bind 0.0.0.0:8000 calculator_project.wsgi:application
+tail -f /dev/null
