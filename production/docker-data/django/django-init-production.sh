48a022042edc29ff984e438266fe30064a713466
ismaelgithub7 <ismaelfernandezmendezfp@gmail.com>
2023-12-06 17:38:00 +0100

    prueba error

--- production/docker-data/django/django-init-production.sh
+++ production/docker-data/django/django-init-production.sh
@@ -3,4 +3,5 @@
 python3 /django-data/manage.py makemigrations
 python3 /django-data/manage.py runserver --settings=calculator_project.settings-production 0.0.0.0:8000
 gunicorn --bind 0.0.0.0:8000 calculator_project.wsgi:application
+adsasda
 tail -f /dev/null
